5e195f010d4e09c8eed74a6edf100c36d2a706c5
Sean Chmielecki <35748130+SeanChmielecki@users.noreply.github.com>
2018-02-06 19:08:51 -0500

    Add files via upload

---
+++ ArrayList/src/Arraylist.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+		ArrayList<String> teachers = new ArrayList<String>();
+		
+		teachers.add("Anthony");
+		teachers.add("Paul");
+		teachers.add("John");
+		teachers.add("Martin");
+		
+		System.out.println( teachers.get(0) );
+		System.out.println( teachers.get(1) );
+		System.out.println( teachers.get(2) );
+		System.out.println( teachers.get(3) );
+	}
+}
